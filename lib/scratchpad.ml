b855cef24be00724793b0b245e12e1c27da3e5ff
Matt Teichman <teichman@uchicago.edu>
2025-07-24 12:37:12 -0500

    blech

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -1,17 +1,29 @@
-type realworld = RealWorld
-
-module State = struct
-  type ('s, 'a) t = 's -> 'a * 's
-  let pure x state = (x, state)
-  let bind mx k state1 =
-    let ( result1, state2 ) = mx state1
-    in k result1 @@ state2
-  let ( let* ) = bind
-end
-
 module ST = struct
-  type 'a st = ST : ('s, 'a) State.t * 's -> 'a st
-  (* let pure = ??? *)
+
+  module State = struct
+    type ('s, 'a) t = 's -> 'a * 's
+    let pure x state = (x, state)
+    let bind mx k state1 =
+      let ( result1, state2 ) = mx state1
+      in k result1 @@ state2
+    let ( let* ) = bind
+  end
+
+  type 'a st = ST : ('s option, 'a) State.t * 's option -> 'a st
+  let runST (ST (stateful, initial)) = fst (stateful initial)
+
+  (* a bit harder than you might think to make this puppy a monad *)
+
+  let pure x =
+    let open State in
+    let statey s = pure x s in
+    ST (statey, None)
+
+  (* let bind (ST (statey, state1)) k = *)
+  (*   let ( result1, state2 ) = statey state1 *)
+  (*   in match k result1 with *)
+  (*        ST (statey2, state3) *)
+
 end
 
 module STRef = struct
