b828242095cdf1d7b408780827668d97bb01269f
Matt Teichman <teichman@uchicago.edu>
2025-07-26 11:29:04 -0500

    got rid of commented out version

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -8,34 +8,13 @@ module ST = struct
   let runST (ST (_, x)) = x
   let pure x = ST (None, x)
   let bind (ST (_, x)) (k : 'a -> 'b st) = k x
+  let ( let* ) = bind
 
   let newstref x = ST (None, STRef (None, ref x))
   let readstref (STRef (s, x)) = ST (s, !x)
   let modifystref (STRef (s, rf)) f =
     rf := f !rf ;
     ST (s, ())
-
-  (* type 'a st = ST : ('s option, 'a) State.t * 's option -> 'a st *)
-  (* let runST (ST (stateful, initial)) = fst (stateful initial) *)
-
-  (* a bit harder than you might think to make this puppy a monad *)
-
-  (* let pure x = *)
-  (*   let open State in *)
-  (*   let statey s = pure x s in *)
-  (*   ST (statey, None) *)
-
-  (* let bind (ST (statey, state1)) k = *)
-  (*   let ( result1, state2 ) = statey state1 in *)
-  (*   match k result1 with *)
-  (*     ST ( statey2, _ ) -> ST (statey2, state2) *)
-
-       (* yeah, kinda got nothing right now for bind *)
-  
-       (* let bind (ST (statey, state1)) k = *)
-       (*   let ( result1, state2 ) = statey state1 *)
-       (*   in match k result1 with *)
-       (*        ST (statey2, state3) *)
 
 end
 
