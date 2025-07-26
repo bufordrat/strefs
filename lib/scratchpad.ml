b467dfdcd4cbaf3378bcb60bd280c48d3a9458c0
Matt Teichman <teichman@uchicago.edu>
2025-07-26 11:29:47 -0500

    remove enclosing ST module

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -1,15 +1,13 @@
-module ST = struct
-  type 'a st = ST : 's * 'a -> 'a st
-  type 'a stref = STRef : 's option * 'a ref -> 'a stref
+type 'a st = ST : 's * 'a -> 'a st
+type 'a stref = STRef : 's option * 'a ref -> 'a stref
 
-  let runST (ST (_, x)) = x
-  let pure x = ST (None, x)
-  let bind (ST (_, x)) (k : 'a -> 'b st) = k x
-  let ( let* ) = bind
+let runST (ST (_, x)) = x
+let pure x = ST (None, x)
+let bind (ST (_, x)) (k : 'a -> 'b st) = k x
+let ( let* ) = bind
 
-  let newstref x = ST (None, STRef (None, ref x))
-  let readstref (STRef (s, x)) = ST (s, !x)
-  let modifystref (STRef (s, rf)) f =
-    rf := f !rf ;
-    ST (s, ())
-end
+let newstref x = ST (None, STRef (None, ref x))
+let readstref (STRef (s, x)) = ST (s, !x)
+let modifystref (STRef (s, rf)) f =
+  rf := f !rf ;
+  ST (s, ())
