267b569f59c14ab5d1d33c938b6fa81ecd274619
Matt Teichman <teichman@uchicago.edu>
2025-07-26 11:36:41 -0500

    replace everything with a record

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -1,13 +1,14 @@
-type 'a st = ST : 's * 'a -> 'a st
-type 'a stref = STRef : 's option * 'a ref -> 'a stref
+type 'a exist = { e : 's. 's option * 'a }
+type 'a st = ST : 'a exist -> 'a st
+type 'a stref = STRef : 'a exist -> 'a stref
 
-let runST (ST (_, x)) = x
-let pure x = ST (None, x)
-let bind (ST (_, x)) (k : 'a -> 'b st) = k x
+let runST (ST { e = _, x }) = x
+let pure x = ST { e = None, x }
+let bind (ST { e = _, x }) (k : 'a -> 'b st) = k x
 let ( let* ) = bind
 
-let newstref x = ST (None, STRef (None, ref x))
-let readstref (STRef (s, x)) = ST (s, !x)
-let modifystref (STRef (s, rf)) f =
+let newstref x = ST { e = None, STRef { e = None, ref x } }
+let readstref (STRef { e = s, x }) = ST { e = s, !x }
+let modifystref (STRef { e = s, rf }) f =
   rf := f !rf ;
-  ST (s, ())
+  ST { e = s, () }
