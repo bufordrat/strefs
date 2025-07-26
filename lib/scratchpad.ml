9382a656f5bbf15e6d0dd6e10bc7f7e4b32c4ca4
Matt Teichman <teichman@uchicago.edu>
2025-07-26 11:28:35 -0500

    lol, still not really working

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -3,11 +3,11 @@ module ST = struct
   type realworld = RW : 'a -> realworld
 
   type 'a st = ST : 's * 'a -> 'a st
-  type 'a stref = STRef : 's * 'a ref -> 'a stref
+  type 'a stref = STRef : 's option * 'a ref -> 'a stref
 
   let runST (ST (_, x)) = x
   let pure x = ST (None, x)
-  let bind (ST (_, x)) k = k x
+  let bind (ST (_, x)) (k : 'a -> 'b st) = k x
 
   let newstref x = ST (None, STRef (None, ref x))
   let readstref (STRef (s, x)) = ST (s, !x)
