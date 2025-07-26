2f82503da54e37527462a4411e9622d21c78ae2f
Matt Teichman <teichman@uchicago.edu>
2025-07-26 11:22:50 -0500

    trying a new tack: implement the whole thing using OCaml refs

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -1,33 +1,44 @@
 module ST = struct
 
-  module State = struct
-    type ('s, 'a) t = 's -> 'a * 's
-    let pure x state = (x, state)
-    let bind mx k state1 =
-      let ( result1, state2 ) = mx state1
-      in k result1 @@ state2
-    let ( let* ) = bind
-  end
+  type realworld = RW : 'a -> realworld
 
-  type 'a st = ST : ('s option, 'a) State.t * 's option -> 'a st
-  let runST (ST (stateful, initial)) = fst (stateful initial)
+  type 'a st = ST : 's * 'a -> 'a st
+  type 'a stref = STRef : 's * 'a ref -> 'a stref
+
+  let runST (ST (_, x)) = x
+  let pure x = ST (None, x)
+  let bind (ST (_, x)) k = k x
+
+  let newstref x = ST (None, STRef (None, ref x))
+  let readstref (STRef (s, x)) = ST (s, !x)
+  let modifystref (STRef (s, rf)) f =
+    rf := f !rf ;
+    ST (s, ())
+
+  (* type 'a st = ST : ('s option, 'a) State.t * 's option -> 'a st *)
+  (* let runST (ST (stateful, initial)) = fst (stateful initial) *)
 
   (* a bit harder than you might think to make this puppy a monad *)
 
-  let pure x =
-    let open State in
-    let statey s = pure x s in
-    ST (statey, None)
+  (* let pure x = *)
+  (*   let open State in *)
+  (*   let statey s = pure x s in *)
+  (*   ST (statey, None) *)
 
-  (* yeah, kinda got nothing right now for bind *)
+  (* let bind (ST (statey, state1)) k = *)
+  (*   let ( result1, state2 ) = statey state1 in *)
+  (*   match k result1 with *)
+  (*     ST ( statey2, _ ) -> ST (statey2, state2) *)
+
+       (* yeah, kinda got nothing right now for bind *)
   
-(* let bind (ST (statey, state1)) k = *)
-  (*   let ( result1, state2 ) = statey state1 *)
-  (*   in match k result1 with *)
-  (*        ST (statey2, state3) *)
+       (* let bind (ST (statey, state1)) k = *)
+       (*   let ( result1, state2 ) = statey state1 *)
+       (*   in match k result1 with *)
+       (*        ST (statey2, state3) *)
 
 end
 
-module STRef = struct
-  type 'a stref = STRef : 's * 'a ref -> 'a stref
-end
+(* module STRef = struct *)
+(*   type 'a stref = STRef : 's * 'a ref -> 'a stref *)
+(* end *)
