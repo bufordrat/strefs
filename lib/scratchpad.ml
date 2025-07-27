d43a33f83a70d3452b66b56870409a130415f5de
Matt Teichman <teichman@uchicago.edu>
2025-07-26 21:52:18 -0500

    hmm, closer but still not getting what I want

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -1,14 +1,15 @@
 type ('s, 'a) st = ST : 'a -> ('s, 'a) st
 type ('s, 'a) stref = STRef : 'a ref -> ('s, 'a) stref
 
-let runST (ST (_, x)) = x
+let runST (ST x) = x
 let pure x = ST (None, x)
 let bind : type s a b. (s, a) st -> (a -> (s, b) st) -> (s, b) st =
   fun (ST x) k -> k x
 let ( let* ) = bind
 
-(* let newstref x = ST (None, STRef (None, ref x)) *)
-(* let readstref (STRef (s, x)) = ST (s, !x) *)
-(* let modifystref (STRef (s, rf)) f = *)
-(*   rf := f !rf ; *)
-(*   ST (s, ()) *)
+let newstref : type s a. a -> (s, (s, a) stref) st =
+  fun x ->  ST (STRef (ref x))
+let readstref (STRef rf) = ST !rf
+let modifystref (STRef rf) f =
+  rf := f !rf ;
+  ST ()
