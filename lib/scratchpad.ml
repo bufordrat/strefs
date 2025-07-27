e59ecf3de76b0e7cdc2fb4b3d57fab59e8bb0cdc
Matt Teichman <teichman@uchicago.edu>
2025-07-26 23:22:15 -0500

    hmm, still experiencing weirdness

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -1,14 +1,20 @@
-type ('s, 'a) st = ST : { st : 's. 'a } -> ('s, 'a) st
+type ('s, 'a) st = ST of 'a
+type 'a run = { run : 's. ('s, 'a) st }
 type ('s, 'a) stref = STRef of 'a ref
 
-let runST (ST { st = x }) = x
-let pure x = ST { st = x }
-let bind : type s a b. (s, a) st -> (a -> (s, b) st) -> (s, b) st =
-  fun (ST { st = x }) k -> k x
-let ( let* ) = bind
-
+let runST = fun run -> run.run
 let newstref : type s a. a -> (s, (s, a) stref) st =
-  fun x -> ST { st = STRef (ref x) }
+  fun x -> ST (STRef (ref x))
+
+(* let x = newstref 12 *)
+let runx = { run = newstref 12 }
+
+(* let pure x = ST x *)
+(* let bind : type s a b. (s, a) st -> (a -> (s, b) st) -> (s, b) st = *)
+(*   fun (ST { st = x }) k -> k x *)
+(* let ( let* ) = bind *)
+
+
 (* let readstref (STRef rf) = ST !rf *)
 (* let modifystref (STRef rf) f = *)
 (*   rf := f !rf ; *)
