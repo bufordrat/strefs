350f4df2041b5f925d1f0047ac6948ddc13d59b9
Matt Teichman <teichman@uchicago.edu>
2025-07-26 23:27:49 -0500

    still not working, will have to puzzle further

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -2,12 +2,13 @@ type ('s, 'a) st = ST of (unit -> 'a)
 type 'a run = { run : 's. unit -> ('s, 'a) st }
 type ('s, 'a) stref = STRef of 'a ref
 
-let runST = fun run -> run.run
+let runST x = match x.run () with
+  | ST f -> f ()
 let newstref : type s a. a -> (s, (s, a) stref) st =
   fun x -> ST (fun () -> (STRef (ref x)))
 
 (* let x = newstref 12 *)
-let runx = { run = fun () -> newstref 12 }
+(* let runx = { run = fun () -> newstref 12 } *)
 
 (* let pure x = ST x *)
 (* let bind : type s a b. (s, a) st -> (a -> (s, b) st) -> (s, b) st = *)
