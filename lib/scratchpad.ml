9e889d3e675b9647d9d8fa8520f0c138a99c8757
Matt Teichman <teichman@uchicago.edu>
2025-07-27 21:45:01 -0500

    add modifystref and stuff

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -1,39 +1,15 @@
-(* type ('s, 'a) st = ST : (unit -> 'a) -> ('s, 'a) st *)
-
 type ('s, 'a) st = ST of (unit -> 'a)
 type ('s, 'a) stref = STRef of 'a ref
-type 'a runst = { runst : 's. unit -> ('s, 'a) st }
+type 'a stthunk = { stthunk : 's. unit -> ('s, 'a) st }
 
 let pure x = ST (fun () -> x)
-let bind (ST k) f = ST (fun () -> f (k ()))
+let bind : type s a b. (s, a) st -> (a -> (s, b) st) -> (s, b) st = 
+  fun (ST x) k -> k (x ())
 let newstref : type s a. a -> (s, (s, a) stref) st =
   fun x -> ST (fun () -> STRef (ref x))
-(* let modifystref (STRef (s, rf)) f = *)
-(*   rf := f !rf ; *)
-(*   ST (s, ()) *)
-let runST x = match x.runst () with ST k -> k ()
-
-(* type 'a runst = { runst : 's. unit -> ('s, 'a) st } *)
-
-(* type 'a stref = STRef : 's option * 'a ref -> 'a stref *)
-
-(* let runST (ST (_, x)) = x *)
-(* let pure x = ST (None, x) *)
-(* let bind (ST (_, x)) (k : 'a -> 'b st) = k x *)
-(* let ( let* ) = bind *)
-
-(* let newstref x = ST (None, STRef (None, ref x)) *)
-(* let readstref (STRef (s, x)) = ST (s, !x) *)
-(* let modifystref (STRef (s, rf)) f = *)
-(*   rf := f !rf ; *)
-(*   ST (s, ()) *)
-
-(* type ('s, 'a) st = ST of (unit -> 'a) *)
-(* type ('s, 'a) stref = STRef of 'a st *)
-
-(* type 'a st = { runst : 's. unit -> ('s, 'a) st } *)
-
-(* let runST (ST k) = k () *)
-(* let pure x = ST (fun () -> x) *)
-(* let bind (ST k) f = ST (fun () -> f (k ())) *)
-(* let newstref x = ST (fun () -> STRef (ref x)) *)
+let modifystref (STRef rf) f =
+  rf := f !rf ;
+  ST (fun () -> ())
+let readstref : type s a. (s, a) stref -> (s, a) st =
+  fun (STRef rf) -> ST (fun () -> !rf)
+let runst x = match x.stthunk () with ST k -> k ()
