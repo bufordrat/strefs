69578eccdc1dbafdf99e1372ba5524d522c0bf5b
Matt Teichman <teichman@uchicago.edu>
2025-07-29 14:16:31 -0500

    textbookify example a bit

--- lib/scratchpad.ml
+++ lib/scratchpad.ml
@@ -1,15 +1,33 @@
 type ('s, 'a) st = ST of (unit -> 'a)
 type ('s, 'a) stref = STRef of 'a ref
-type 'a stthunk = { stthunk : 's. unit -> ('s, 'a) st }
 
 let pure x = ST (fun () -> x)
-let bind : type s a b. (s, a) st -> (a -> (s, b) st) -> (s, b) st = 
+
+let bind : ('s, 'a) st -> ('a -> ('s, 'b) st) -> ('s, 'b) st = 
   fun (ST x) k -> k (x ())
-let newstref : type s a. a -> (s, (s, a) stref) st =
-  fun x -> ST (fun () -> STRef (ref x))
+
+let ( let* ) = bind
+
+let newstref : 'a -> ('s, ('s, 'a) stref) st
+  = fun x -> ST (fun () -> STRef (ref x))
+
 let modifystref (STRef rf) f =
   rf := f !rf ;
   ST (fun () -> ())
-let readstref : type s a. (s, a) stref -> (s, a) st =
+
+let readstref : ('s, 'a) stref -> ('s, 'a) st =
   fun (STRef rf) -> ST (fun () -> !rf)
-let runst x = match x.stthunk () with ST k -> k ()
+
+type 'a stthunk = { stthunk : 's. unit -> ('s, 'a) st }
+
+let runst x = match x.stthunk () with ST thunk -> thunk ()
+
+let succ_it =
+  let* rf = newstref 0 in
+  let* () = modifystref rf succ in
+  readstref rf
+
+let succ_it_ref =
+  let* rf = newstref 0 in
+  let* () = modifystref rf succ in
+  pure rf
